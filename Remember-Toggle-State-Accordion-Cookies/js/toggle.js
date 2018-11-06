09874eb286ec94ba0e5c27e7f299bb490c1f917e
Nurana Rustamova <nuranaar@code.edu.az>
2018-11-06 11:44:25 +0400

    change parts

---
+++ Remember-Toggle-State-Accordion-Cookies/js/toggle.js
@@ -0,0 +1,40 @@
+$(document).ready(function () {
+    if (document.cookie.indexOf('collapsed_Nodes') >= 0) {
+        var getCollapsed = Cookies.get('collapsed_Nodes');
+        var checkCollapsed = getCollapsed.split('|');
+        var arrayLength = checkCollapsed.length;
+        for (var i = 0; i < arrayLength; i++) {
+            $('[data-node="#' + checkCollapsed[i] + '"]').next('.accordion_body').addClass('hide');
+            $('[data-node="#' + checkCollapsed[i] + '"]').children('.plusminus').children('img').attr('src', './icon/plus.svg');
+        }
+    }
+
+    $('.accordion_head').click(function () {
+        if (!$(this).next('.accordion_body').hasClass('hide')) {
+            $(this).next('.accordion_body').slideDown(300);
+            $(this).next('.accordion_body').addClass('hide');
+            $(this).css({
+                "background-color": "#f2963f",
+            });
+            $(this).find(".plusminus").text("-");
+            if ($(this).parent().siblings().find(".hide")) {
+                $(this).parent().siblings().find(".accordion_body").slideUp(300);
+                $(this).parent().siblings().find(".plusminus").text("+");
+                $(this).parent().siblings().find(".accordion_head").css({
+                    "background-color": "#444444",
+                    "color": "white"
+                });
+            }
+        } else {
+            $(this).next('.accordion_body').slideUp(300);
+            $(this).next('.accordion_body').removeClass('hide');
+            $(this).find(".plusminus").text("+");
+            $(this).css({
+                "background-color": "#444444",
+                "color": "white"
+            });
+
+        }
+
+    });
+});
